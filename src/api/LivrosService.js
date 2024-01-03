0c87f5f9ad95d3b076397358ca08205adb9d10da
dsato39 <dsato39@gmail.com>
2024-01-03 15:22:26 -0300

    adicionando url do back end

--- src/api/LivrosService.js
+++ src/api/LivrosService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080"
+const BASE_URL = "https://dnc-desafio5-library-back.vercel.app/"
 
 export class LivrosService{
     static getLivros(){

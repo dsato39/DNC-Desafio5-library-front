f7d741e626f146ddf314f0ae7c4e06558640881a
dsato39 <dsato39@gmail.com>
2024-01-03 12:28:32 -0300

    first commit

---
+++ src/api/LivrosService.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:8080"
+
+export class LivrosService{
+    static getLivros(){
+        return axios.get(BASE_URL+'/livros');
+    }
+
+    static getLivro(id){
+        return axios.get(`${BASE_URL}/livros/edicao/${id}`);
+    }
+
+    static createLivro(body){
+        return axios.post(`${BASE_URL}/livros/cadastro`,body);
+    }
+
+    static updateLivro(id,body){
+        return axios.put(`${BASE_URL}/livros/edicao/${id}`,body);
+    }
+
+    static deleteLivro(id){
+        return axios.delete(`${BASE_URL}/livros/${id}`);
+    }
+    
+}
